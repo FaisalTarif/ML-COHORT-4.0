d8cb1a5dbc2254a2af600c8eecbf8313b0505c5b
Faisal Nabib Tarif <148097108+FaisalTarif@users.noreply.github.com>
2025-05-08 20:44:28 +0600

    Add files via upload

---
+++ Tasks/task_1.py
@@ -0,0 +1,28 @@
+#Problem 1
+x = "ABRACADABRA"
+
+print("Problem 1: ")
+print(x[2])
+
+
+#Problem 2
+y = "The quick brown fox jumps over the lazy dog"
+print("Problem 2: ")
+print(y[10:20])
+
+
+#Problem 3
+A = "বাংলা"
+B = "আমার"
+C = "সোনার"
+D = B + " " + C + " " + A
+print("Problem 3: ")
+print(D)
+
+
+#Problem 4
+print("Problem 4: ")
+singer = input("Your favourite singer? : ")
+song = input("His most popular song? : ")
+print(f"My favourite singer is {singer}, whose most popular song is {song}")
+
